d8bf3d69843c3a771bb362213714a275dc79fb41
Raymond Dai <RaymondD214@users.noreply.github.com>
2022-12-23 15:50:05 -0500

    Update Solution.java

--- LongestRepeating/Solution.java
+++ LongestRepeating/Solution.java
@@ -3,23 +3,28 @@ import java.util.LinkedList;
 import java.util.Set;
 
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
+    public int lengthOfLongestSubstring(String s, int k) {
         int max = 0;
         int j = 0;
         int i = 0;
+        int temp = 0;
+        char thing = s.charAt(0);
 
-        if (s == null || s.length() == 0) return 0;
-        Set<Character> set = new HashSet<>();
+        if (s == null || s.length() == 0 || k <= 0 || k >= 100000) return 0;
+        // Set<Character> set = new HashSet<>();
         LinkedList list = new LinkedList<>();
+        
 
         while (i < s.length()){
             char c = s.charAt(i);
-            while(set.contains(c)){
+            while(temp > k){
 
-                set.remove(s.charAt(j));
+                // set.remove(s.charAt(j));
+                list.remove(s.charAt(j));
                 ++j;
             }
-            set.add(c);
+            // set.add(c);
+            list.add(c);
             max = Math.max(max, i - j +1);
             ++i;
         }
