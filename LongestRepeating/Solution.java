8a41791b8caf11635832f8eb008c5a7ed186a596
Raymond Dai <RaymondD214@users.noreply.github.com>
2022-12-23 15:12:13 -0500

    Create Solution.java

---
+++ LongestRepeating/Solution.java
@@ -0,0 +1,29 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int max = 0;
+        int j = 0;
+        int i = 0;
+
+        if (s == null || s.length() == 0) return 0;
+        Set<Character> set = new HashSet<>();
+        LinkedList list = new LinkedList<>();
+
+        while (i < s.length()){
+            char c = s.charAt(i);
+            while(set.contains(c)){
+
+                set.remove(s.charAt(j));
+                ++j;
+            }
+            set.add(c);
+            max = Math.max(max, i - j +1);
+            ++i;
+        }
+
+        return max;
+    }
+}
